8f21f738bcc9e544c6c52c10dce0731868c0c68f
James Brown <jameseyb@gmail.com>
2016-04-28 13:22:15 +0100

    Function created for limited OS detection (Python 2.7, Windows/Linux only)

--- osDetect.py
+++ osDetect.py
@@ -5,18 +5,20 @@
 print("Detecting operating system...")
 
 import os
-#from sys import argv
 
-winPyPath = 'C:\Program Files (x86)\Python2.7'
-linPyPath = '/usr/lib/python2.7'
+def osType():
+	winPyPath = 'C:\Program Files (x86)\Python2.7'
+	linPyPath = '/usr/lib/python2.7'
 
-print(winPyPath)
-print(linPyPath)
-
-pyPathExists = os.path.exists(winPyPath)
-print(pyPathExists)
+	winPyPathExists = os.path.exists(winPyPath)
+	linPyPathExists = os.path.exists(linPyPath)
 	
+	if winPyPathExists == True:
+		print("Currently running on Windows")
+	elif winPyPathExists == False & linPyPathExists == True:
+		print("Currently running on Linux")
+	else: 
+		print("Error: OS not supported")	
 
-pyPathIsDir = os.path.isdir('C:\Program Files (x86)\Python2.7')
-print(pyPathIsDir)
+osType()
 
