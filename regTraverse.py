d30a3a4d15d6a75fdcd957dacf0d0521bd02ffe4
James Brown <jameseyb@gmail.com>
2016-05-06 13:38:24 +0100

    Actually works for a single, specified registry key.
    Configured to outut to a flie rather than to the screen.

--- regTraverse.py
+++ regTraverse.py
@@ -7,6 +7,9 @@ from __future__ import unicode_literals, print_function
 import os
 import sys
 import winreg
+from sys import argv
+
+script, inputname = argv
 
 #Functions here
 def versionCheck(): # Checks the version of python installed
@@ -23,21 +26,29 @@ def subkeys(key): #Function iterates over the keys in the current registry folde
 			subkey = winreg.EnumKey(key, i)
 			yield subkey
 			i+=1
-		except WindowsError as e:
-			break		
-
+		except OSError as e:
+			return None
+			
+def printValues(hkey): #Returns values in regitry keys
+	for i in range(winreg.QueryInfoKey(hkey)[1]):
+		n,v,t = winreg.EnumValue(hkey,i)
+		of1.write('\t' + str(n) + ',' + str(v) +',' + str(t) + '\n')
+	
 def traverse_registry_tree(hkey, keypath): #Function walks down the hive and writes the registry keys to a file.
 	try:
 		key = winreg.OpenKey(hkey, keypath, 0, winreg.KEY_READ)
+		printValues(key)
 		for subkeyname in subkeys(key):
 			of1.write(keypath + '\\' + subkeyname + '\n')
-			for i in range(winreg.QueryInfoKey(key)[1]):
-				n,v,t = winreg.EnumValue(key,i)
-				of1.write('\t' + str(n) + ',' + str(v) +',' + str(t) + '\n')
-			subkeypath = "%s\\%s" % (keypath, subkeyname)
-			traverse_registry_tree(hkey, subkeypath)
+			try:
+				subkeypath = "%s\\%s" % (keypath, subkeyname)
+				traverse_registry_tree(hkey, subkeypath)
+			except:
+				raise
 	except WindowsError as e:
-		print("Specified path in registry (" + keypath + ") does not exist\n")
+		raise
+	finally:
+		winreg.CloseKey(key)
 
 #Pre-script checks
 versionCheck()		
@@ -46,11 +57,12 @@ versionCheck()
 if __name__ == '__main__':
 
 	#Connect to registry and correct location
-	inputname = str(input("Enter string to search for: "))
-	keypath = str(r"Software" + "\\" + inputname)
+	keypath = str("Software" + "\\" + inputname)
 	print("Path found: " + keypath)
 	
 	#Walk first registry entry
 	of1 = open(inputname + "_HKCU" + ".log", "w")
-	traverse_registry_tree(winreg.HKEY_CURRENT_USER, keypath)
-	of1.close()
+	try:
+		traverse_registry_tree(winreg.HKEY_CURRENT_USER, keypath)
+	finally:
+		of1.close()
